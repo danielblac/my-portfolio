fe590162ec94c4537144e66208171d4ba833f04f
danielblac <daniel_blac@yahoo.com>
2023-05-30 18:50:34 +0100

    Fixed Contact Mobile View

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,7 +15,7 @@ export default function Contact() {
           <div className="contact-details flex flex-col items-center pt-3">
             <h2 className='text-3xl text-center font-bold leading-snug max-w-3xl xs:leading-snug xs:text-5xl sm:text-6xl sm:leading-snug xl:text-7xl xl:leading-snug xl:-mt-3'>I'd love very much to hear from you...</h2>
             <p className='font-mono text-sm italic xs:text-lg sm:text-xl lg:pt-2 xl:text-2xl xl:pt-0'>Feel free to contact me on 👇</p>      
-            <div className="social-icons w-10/12 text-md flex flex-col flex-wrap max-w-3xl justify-center gap-7 my-5 xs:my-9 xs:gap-9 xs:text-2xl sm:flex-row sm:w-full sm:text-3xl sm:gap-12 sm:my-10 lg:font-semibold lg:text-4xl lg:gap-x-16 xl:gap-x-20 xl:gap-y-10">
+            <div className="social-icons w-10/12 text-md flex flex-col flex-wrap max-w-3xl justify-center gap-7 my-5 xs:my-9 xs:gap-9 xs:text-2xl sm:text-3xl sm:gap-12 md:flex-row md:w-full md:my-10 lg:font-semibold lg:text-4xl lg:gap-x-16 xl:gap-x-20 xl:gap-y-10">
               <Link href="tel:+2348165044450"  className="contact-links"><FaMobileAlt /> Call me</Link>
               <Link href="mailto:daniel_blac@yahoo.com" className="contact-links"><FaEnvelope /> Send a Mail</Link>
               <Link href="https://linkedin.com/in/daniel-egboro-ebipamowei" target='_blank' rel="noopener noreferrer" className="contact-links"><FaLinkedin /> LinkedIn</Link>
