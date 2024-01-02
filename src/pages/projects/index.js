bcb2324c37288f30b64cfff0a2a015715a198605
danielblac <daniel_blac@yahoo.com>
2024-01-02 01:43:01 +0100

    Updated projects

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -58,18 +58,18 @@ export default function Projects() {
             </div>
           </div>
         </Link>
-        <Link href='https://daniel-movie-search-app.netlify.app' target="_blank" rel="noopener noreferrer">
+        <Link href='https://www.split-delivery.com' target="_blank" rel="noopener noreferrer">
           <div className="bg-skill py-7 rounded-xl mb-5 flex flex-col px-1.5 items-center md:flex-row-reverse md:gap-3 md:px-4 xl:px-8">
             <Image 
               className="w-96 h-64 rounded-xl mx-8 px-5 md:mx-0 md:px-0 md:w-80 md:h-72 lg:w-96 lg:h-80 xl:w-105 xl:h-80 xxl:max-w-none xxl:w-110"
-              src='/image/movie-watchlist.png'
-              alt="inshorens-landing-page"
+              src='/image/split-delivery.png'
+              alt="split-delivery"
               width={750}
               height={600}
             />
             <div className='pt-2 px-1.5 text-center md:text-left lg:px-4 lg:mr-4'>
-              <h3 className='font-bold text-2xl mt-3 sm:text-3xl md:text-2xl md:mt-0 lg:text-3xl xl:text-4xl xxl:text-5xl xxl:leading-normal'>Movie Watchlist</h3>
-              <p className='text-sm tracking-wide pt-2 md:pt-1 xl:text-lg lg:pt-2'>This Website allows you to search for the information of both known and unknown movies and save them on your watchlist which you can access any other time. Basically improves your movie search experience.</p>
+              <h3 className='font-bold text-2xl mt-3 sm:text-3xl md:text-2xl md:mt-0 lg:text-3xl xl:text-4xl xxl:text-5xl xxl:leading-normal'>Split Delivery</h3>
+              <p className='text-sm tracking-wide pt-2 md:pt-1 xl:text-lg lg:pt-2'>This is a website i built for a client, It has a landing page and an admin panel, The admin panel has authentication which is did using next-auth, I worked on this project with a backend developer who worked on all the endpoints needed to fulfill the customer's requirements.</p>
             </div>
           </div>
         </Link>
